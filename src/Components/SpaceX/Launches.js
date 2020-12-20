203754c5edbc3d6926118c535edbb014b26aa1cb
Carl Redding <credding07@gmail.com>
2020-12-20 00:35:38 -0500

    some reason my api from spacex isnt bringing in the photos as promised.

--- src/Components/SpaceX/Launches.js
+++ src/Components/SpaceX/Launches.js
@@ -37,23 +37,38 @@ export default function Launches() {
     return (
       <div className='payload-container'>
         {/* <img className='patch' src={launches.links.patch.large} alt='png' /> */}
-        <h1>Name of Launch: {launches.name}</h1>
+        <h1>
+          Name of Launch: <br />
+          {launches.name}
+        </h1>
         <div className='image-container'>
-          {/* <img
-            className='launch-image'
-            src={launches.links.flickr.original[0]}
-            alt='jpg'
-          />
           <img
             className='launch-image'
-            src={launches.links.flickr.original[1]}
+            // src={launches.links.flickr.large[0]}
+            src={launches.links}
             alt='jpg'
-          /> */}
+          />
+          {console.log(launches.links)}
+          <img
+            className='launch-image'
+            // src={launches.links.flickr.large[1]}
+            // src={links.patch.small}
+            alt='jpg'
+          />
         </div>
 
-        <p>Local Date and Time of launch: {launches.date_local}</p>
-        <p>Flight Number: {launches.flight_number}</p>
-        <p>Mission objective: {launches.details}</p>
+        <p>
+          Local Date and Time of launch: <br />
+          {launches.date_local}
+        </p>
+        <p>
+          Flight Number: <br />
+          {launches.flight_number}
+        </p>
+        <p>
+          Mission objective: <br />
+          {launches.details}
+        </p>
 
         {/* <button src={launches.links.webcast}>Youtube link</button> */}
       </div>
